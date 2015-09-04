44644899b9fb77aefb57346e19c7757fec6386bf
Jimmy Huang <jimmy@netivism.com.tw>
2015-09-05 03:25:12 +0800

    Better cron run script. Will consider multi-site structure of drupal. refs #16325

--- playbooks/roles/neticrm/files/drupal_cron.sh
+++ playbooks/roles/neticrm/files/drupal_cron.sh
@@ -1,26 +1,33 @@
 # !/bin/bash
 
 docker_cron_run(){
-  docker exec -i $1 bash -c "cd /var/www/html && drush cron --quiet"
+  docker exec -i $1 bash -c "cd /var/www/html && drush -l $2 cron --quiet"
 }
 
 docker_cron_run_neticrm(){
-  docker exec -i $1 bash -c "cd /var/www/html && drush neticrms run_civimail"
-  docker exec -i $1 bash -c "cd /var/www/html && drush neticrms run_civimail_process"
-  docker exec -i $1 bash -c "cd /var/www/html && drush neticrms run_contact_greeting_update"
-  docker exec -i $1 bash -c "cd /var/www/html && drush neticrms run_participant_processor"
-  docker exec -i $1 bash -c "cd /var/www/html && drush neticrms run_membership_status_update"
+  docker exec -i $1 bash -c "cd /var/www/html && drush -l $2 neticrms run_civimail"
+  docker exec -i $1 bash -c "cd /var/www/html && drush -l $2 neticrms run_civimail_process"
+  docker exec -i $1 bash -c "cd /var/www/html && drush -l $2 neticrms run_contact_greeting_update"
+  docker exec -i $1 bash -c "cd /var/www/html && drush -l $2 neticrms run_participant_processor"
+  docker exec -i $1 bash -c "cd /var/www/html && drush -l $2 neticrms run_membership_status_update"
 }
 
 # make sure we have 
 COUNT=0
-LIMIT_NUM=3
+LIMIT_NUM=5
 LIMIT_MIN=30
 
-# find /var/www/sites/*/cron.php -mmin +10 -printf "%C@ %p\n" | sort | awk '{ print $2 }'
-for DIR in `find /var/www/sites/*/cron.php -mmin +$LIMIT_MIN -printf "%C@ %p\n" | sort | awk '{ print $2 }'` ; do
-  BASE=${DIR%/cron.php}
-  NAME=${BASE//\/var\/www\/sites\//}
+for RUN in `find /var/www/sites/*/sites/*/settings.php -mmin +$LIMIT_MIN -printf "%C@ %p\n" | sort | awk '{ print $2 }'` ; do
+  NAME=${RUN//\/var\/www\/sites\//}
+  NAME=${NAME%%/*}
+  TMP=${RUN%/*}
+  TMP=${TMP##*/} 
+  SITE=""
+  if [ "$TMP" != "default" ]; then
+    SITE=$TMP
+  else
+    SITE=$NAME
+  fi
   RUNNING=$(docker ps -q -f name=$NAME)
   if [ -n "$RUNNING" ]; then
     if [ $COUNT -gt $LIMIT_NUM ]; then
@@ -28,9 +35,12 @@ for DIR in `find /var/www/sites/*/cron.php -mmin +$LIMIT_MIN -printf "%C@ %p\n" 
       echo "Excceed $LIMIT_NUM of sites in each cron"
       break
     fi
-    docker_cron_run $NAME
+    echo "$(date +"%Y-%m-%d %H:%M:%S") $NAME cron run" >> /var/log/drupal_cron.log
+    touch $RUN
+    docker_cron_run $NAME $SITE
     if [ -f $BASE/sites/all/modules/civicrm/civicrm-version.txt ]; then
-      docker_cron_run_neticrm $NAME
+      echo "$(date +"%Y-%m-%d %H:%M:%S") $NAME civicrm cron run" >> /var/log/drupal_cron.log
+      docker_cron_run_neticrm $NAME $SITE
     fi
     COUNT=$(($COUNT+1))
   fi
