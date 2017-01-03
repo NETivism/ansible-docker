14dfc797ca18a11b8fcbe468b6ecf31d1c17a08c
Jimmy Huang <jimmy@netivism.com.tw>
2017-01-03 22:42:48 +0800

    Add upgrade script support.

--- playbooks/roles/neticrm/files/neticrm_update.sh
+++ playbooks/roles/neticrm/files/neticrm_update.sh
@@ -13,7 +13,11 @@ for RUN in `find /var/www/sites/*/sites/*/civicrm.settings.php` ; do
   fi
   RUNNING=$(docker ps -q -f name=$NAME)
   if [ -n "$RUNNING" ]; then
-    docker exec -i $NAME bash -c "drush -l $SITE updb --yes &> /var/www/html/log/neticrm_update.log"
+    if [ "$1" -eq '--updb' ]; then
+      docker exec -i $NAME bash -c "drush -l $SITE updb --yes &> /var/www/html/log/neticrm_update.log"
+    fi
+    docker exec -i $NAME bash -c "drush -l $SITE neticrm-clear-cache --yes"
+    docker exec -i $NAME bash -c "drush -l $SITE neticrm-rebuildmenu --yes"
     docker exec -i $NAME bash -c "drush -l $SITE drush cache-clear drush --yes"
   fi
 done
