e938bcb0fc030d7eaa1598ee61e5ae957ece41ba
Jimmy Huang <jimmy@netivism.com.tw>
2017-02-06 09:19:16 +0800

    Do not clear drupal cache when neticrm update

--- playbooks/roles/neticrm/files/neticrm_update.sh
+++ playbooks/roles/neticrm/files/neticrm_update.sh
@@ -22,7 +22,5 @@ for RUN in `find /var/www/sites/*/sites/*/civicrm.settings.php` ; do
     fi
     docker exec -i $NAME bash -c "drush -l $SITE neticrm-clear-cache --yes"
     docker exec -i $NAME bash -c "drush -l $SITE neticrm-rebuildmenu --yes"
-    docker exec -i $NAME bash -c "drush -l $SITE cache-clear drush --yes"
-    docker exec -i $NAME bash -c "drush -l $SITE cache-clear all --yes"
   fi
 done
