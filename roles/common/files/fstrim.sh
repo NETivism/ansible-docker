a42f624b40475a8f1e07a8ec6ce82391a084cba0
Jimmy Huang <jimmy@netivism.com.tw>
2019-04-10 12:13:01 +0800

    Fixes fstrim not work when there is container stopped.

--- roles/common/files/fstrim.sh
+++ roles/common/files/fstrim.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-/usr/bin/docker ps -qa | xargs /usr/bin/docker inspect --format='{{ .State.Pid }}' | xargs -IZ /sbin/fstrim /proc/Z/root/
+PIDS=`/usr/bin/docker ps -qa | xargs /usr/bin/docker inspect --format='{{ .State.Pid }}'`
+
+for PID in $(echo $PIDS); do
+  if [ "$PID" -ne "0" ]; then
+    /sbin/fstrim /proc/$PID/root/
+  fi
+done
